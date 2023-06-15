cc3230924d9ee3e30f7c033dde9a85a4873ea768
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-06-15 19:48:59 +0100

    added allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hammerhead-app-jvcll.ondigitalocean.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
