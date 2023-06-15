91e43d0edc936432450004a09bfe9d8ee086b19b
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-06-15 14:42:22 +0100

    added deployment configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-552!sbp8=_)072$8_#z1u4bw9%z@hb!ci5ml%xu(2j$$ehz4!4'
-SECRET_KEY = os.environ.get('DB_USER')
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
